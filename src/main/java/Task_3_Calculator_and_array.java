919093f8d6fc0ddbfe2309e3d931a9be1175c63b
Petr <mpshigh@gmail.com>
2020-02-02 01:18:46 +0300

    123

--- src/main/java/Task_3_Calculator_and_array.java
+++ src/main/java/Task_3_Calculator_and_array.java
@@ -1,7 +1,4 @@
-import java.io.IOException;
-import java.util.Scanner;
 /** Задание №3
-
  1. Реализовать базовые операции калькулятора (+,-,/,*) для предыдущего задания
  2. Поиск максимального элемента в массиве. Для начала задать массив слов.Размерность массива произвольна, задается
  в консоли. После чего в консоли вводятся слова в количестве равном заданной длине массива. В полученном массиве
@@ -9,53 +6,107 @@ import java.util.Scanner;
  3. Программа должна выполнять одно из заданий на выбор. Если в консоли ввести 1 - запуститься выполнение калькулятора,
  если 2 - поиск максимального слова в массиве.*/
 
-public class Task_3_Calculator_and_array {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
-        public static void main(String[] args) throws IOException {
-            double value1;
-            double value2;
-            System.out.println("Введите \"1\" чтобы запустить выполнение калькулятора");
-            System.out.println("Введите \"2\" чтобы осуществить поиск максимального слова в массиве");
+public class Task_Calculator_and_array
+{
 
-            Scanner scaner1 = new Scanner(System.in);
-            String action = scaner1.nextLine();
+    public static void main(String[] args) throws IOException {
 
-            if (action.equals("1")){
+        System.out.println("Введите \"1\" чтобы запустить выполнение калькулятора");
+        System.out.println("Введите \"2\" чтобы осуществить поиск максимального слова в массиве");
 
-               }
+        Scanner scaner1 = new Scanner(System.in);
+        String action = scaner1.nextLine();
 
-            else if (action.equals("2")){
-
-            }
+        if (action.equals("1")) {
+            double num1 = getNumber();
+            double num2 = getNumber();
+            char operation = getOperation();
+            double result = calc(num1, num2, operation);
+            System.out.println("Результат:" + result);
         }
 
 
+        else if (action.equals("2")){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Определите размер массива");
+            // объявляем массив
+            int a = Integer.parseInt(reader.readLine());
+            String[] array = new String[a];
 
 
-        public static double getNumber () {
-                System.out.println("Введите число");
-                Scanner scaner1 = new Scanner(System.in);
-                if (scaner1.hasNextDouble()) {
-                    return scaner1.nextDouble();
-                } else {
-                    System.out.println("Ошибка при вводе. Повторите ввод");
-                    return getNumber();
+            Scanner sc = new Scanner(System.in);
+            //Вводим предложение в консоль
+            System.out.println("Введите слова одной строкой через пробел");
+            String input = sc.nextLine();
+            //Начальное количество слов равно 0
+
+            String str = "find the most max length in string"; // это исходная строка
+            array = str.split(" "); // тут делим ее на отдельные слова, по пробелу
+            String maxLen = array[0]; // Здесь предполагаем что первый элемент в массиве самый короткий
+
+// Тут последовательно сравниваем длину элементов массива, и если есть короче чем в переменной minLen, заносим ее туда
+
+            for (String e : array) {
+                if (e.length() > maxLen.length()) {
+                    maxLen = e;
                 }
             }
 
+            System.out.print(maxLen); // тут вывод на экран
+        }
 
-        public static char getOperation () {
-            Scanner scaner2 = new Scanner(System.in);
-            String action = scaner2.nextLine();
-
-            System.out.println("Введите требуемую операцию:");
-            System.out.println("\"+\" - сложение,  \"-\" - вычитание");
-            System.out.println("\"*\" - умножение, \"/\" - деление");
+        else {
+            System.out.println("Ошибка при вводе. Повторите ввод");
         }
 
 
+    }
 
-        public static double calculator () {
+
+    public static double getNumber () {
+        System.out.println("Введите число");
+        Scanner scaner1 = new Scanner(System.in);
+        if (scaner1.hasNextDouble()) {
+            return scaner1.nextDouble();
+        } else {
+            System.out.println("Ошибка при вводе. Повторите ввод");
+            return getNumber();
+        }
+    }
+
+
+
+    public static char getOperation() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выберите номер операции:\n1 - прибавить\n2 - отнять\n3 - умножить\n4 - разделить");
+        int operationNumber = 0;
+        if(sc.hasNextInt()) {
+            operationNumber = sc.nextInt();
+        } else {
+            System.out.println("Вы ввели не число! Повторите ввод!");
+            return getOperation();
+        }
+        switch (operationNumber) {
+            case 1:
+                return '+';
+            case 2:
+                return '-';
+            case 3:
+                return '*';
+            case 4:
+                return '/';
+            default:
+                System.out.println("Неправильная операция! Повторите ввод!");
+                return getOperation();
+        }
+    }
+
+    public static double calculator () {
         if (action.equals("+")){
             double result = value1 + value2;
             return result;}
@@ -71,8 +122,8 @@ public class Task_3_Calculator_and_array {
         else if (action.equals("/")){
             double result = value1 / value2;
             return result;}
-        }
-
-
     }
 
+
+
+}
