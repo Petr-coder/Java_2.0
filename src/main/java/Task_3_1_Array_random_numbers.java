52056968140c0615816c88546db33955134d577d
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-02-06 17:44:47 +0300

    Проба пера

---
+++ src/main/java/Task_3_1_Array_random_numbers.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* Массив размерностью 20, заполняется случайными целыми числами от -10 до 10. Найти максимальный отрицательный
+и минимальный положительный элементы массива. Поменять их местами.*/
+
+public class Task_3_1_Array_random_numbers {
+
+    public static void main(String[] args) {
+        int a = (int) (Math.random()*21 - 10);
+        int b = (int) (Math.random()*21 - 10);
+        int c = (int) (Math.random()*21 - 10);
+        int d = (int) (Math.random()*21 - 10);
+        int e = (int) (Math.random()*21 - 10);
+        int f = (int) (Math.random()*21 - 10);
+
+    System.out.println(a);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+
+}
