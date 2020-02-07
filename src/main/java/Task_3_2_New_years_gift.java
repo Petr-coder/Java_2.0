a25aa59aed214fdfdd251bba3b036b025d608ef2
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-02-07 13:52:06 +0300

    Проба пера

---
+++ src/main/java/Task_3_2_New_years_gift.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* Формируется новогодний подарок. Он может включать в себя разные сладости (Candy, Jellybean, etc.) У каждой сладости
+есть название, вес, цена и свой уникальный параметр. Необходимо собрать подарок из сладостей. Найти общий вес подарка,
+общую стоимость подарка и вывести на консоль информацию о всех сладостях в подарке.
+ */
+
+
+public class Task_3_2_New_years_gift {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
