23b8bbbe2ad61f5171467dca8d878d53fc04e321
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-02-07 13:07:39 +0300

    Проба пера

--- src/main/java/Task_3_1_Array_random_numbers.java
+++ src/main/java/Task_3_1_Array_random_numbers.java
@@ -11,13 +11,14 @@ public class Task_3_1_Array_random_numbers {
     public static void main(String[] args) {
     int arrayRandom [] = new int[20];
 
+        //создаем массив и выводим на экран
         for (int i = 0; i < arrayRandom.length; i++){
             arrayRandom [i] = (int) (Math.random()*21 - 10);
-            System.out.print(arrayRandom[i] + " ");
+            System.out.print(arrayRandom[i] + "  ");
         }
 
 
-
+        //ищем миниммальное и максимальное значения. хатем печатаем их
         int maxValue = arrayRandom[0];
         int minValue = arrayRandom[0];
 
@@ -33,8 +34,49 @@ public class Task_3_1_Array_random_numbers {
 
         }
 
+        System.out.println("\n" + "maxValue" + " " + maxValue + ";" + " " + "minValue" + minValue);
 
-        System.out.println("maxValue" + maxValue + ";" +" " + "minValue"+ minValue);
+
+
+        //Находим индекс максимального значения
+        int max = arrayRandom[0];
+        int indexForMax = 0;
+        for (int i = 0; i < arrayRandom.length; i++) {
+            int score = arrayRandom[i];
+            if (max < score) {
+                max = score;
+                indexForMax=i;
+            }
+        }
+
+        //Находим индекс минимального значения
+        int min = arrayRandom[0];
+        int indexForMin = 0;
+        for (int i = 0; i < arrayRandom.length; i++) {
+            int score = arrayRandom[i];
+            if (min > score) {
+                min = score;
+                indexForMin=i;
+            }
+        }
+
+
+
+        System.out.println("indexForMin" + " " + indexForMin + ";" + " " + "indexForMax" + " " + indexForMax);
+
+        //меняем индексы местами
+        int a = 0;
+        a = arrayRandom [indexForMin];
+        arrayRandom [indexForMin] = arrayRandom [indexForMax];
+        arrayRandom [indexForMax] = a;
+
+        //печатаем массив с индкесами поменяными местами
+        for (int i = 0; i < arrayRandom.length; i++){
+
+            System.out.print(arrayRandom[i] + "  ");
+        }
+
+
     }
 
 }
