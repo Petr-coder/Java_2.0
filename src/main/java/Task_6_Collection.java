96da9bff81934296fd676c5e122f6053c79e35b2
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-02-18 18:13:45 +0300

    Проба пера

--- src/main/java/Task_6_Collection.java
+++ src/main/java/Task_6_Collection.java
@@ -1,10 +1,7 @@
 import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  Есть входной файл с набором слов, написанных через пробел
@@ -16,50 +13,28 @@ import java.util.Scanner;
  Найти слово с максимальным количеством повторений. Вывести на консоль это слово и сколько раз оно встречается в файле
  */
 
+
 public class Task_6_Collection {
 
     public static void main(String[] args) throws IOException {
+
     }
 
 
     // считываем файл в Arralist
     public static void getInformation() throws FileNotFoundException {
-        Scanner s = new Scanner(new File("file.txt"));
-        ArrayList<String> list = new ArrayList<String>();
-        while (s.hasNext()){
-            list.add(s.next());
+        Scanner scanner = new Scanner(new File("file.txt"));
+        Map<String, Integer> statistics = new HashMap<>();
+        while (scanner.hasNext()) {
+            String word = scanner.useDelimiter ("\\s+").next();
+            Integer count = statistics.get(word);
+            if (count == null) {
+                count = 0;
+
+            }
+            statistics.put(word, ++count);
         }
-        s.close();
-    }
-
-
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Определите размер массива");
-        // объявляем массив
-        int a = Integer.parseInt(reader.readLine());
-        String[] array = new String[a];
-
-
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите слова одной строкой через пробел");
-        String input = sc.nextLine();
-
-        array = input.split(" ");
-        String maxLen = array[0];
-
-
-
-
-
-
-
-        ArrayList<String> solarSystem = new ArrayList<>(Arrays.asList(mercury, venus, earth, mars,
-                jupiter, saturn, uranus, neptune));
-        Collections.sort(solarSystem);
-        System.out.println(solarSystem);
-
+        System.out.println(statistics);
 
 
 
@@ -69,5 +44,3 @@ public class Task_6_Collection {
     }
 
 
-
-}
