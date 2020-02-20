14a2bdddaa7af777c7dbb6c815e049f9030371d9
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-02-20 16:14:31 +0300

    Проба пера

--- src/main/java/Task_6_Collection.java
+++ src/main/java/Task_6_Collection.java
@@ -1,15 +1,14 @@
 import java.io.*;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.*;
 
 /**
  Есть входной файл с набором слов, написанных через пробел
  Необходимо:
- Прочитать слова из файла.
- Отсортировать в алфавитном порядке.
- Посчитать сколько раз каждое слово встречается в файле.
- Вывести статистику на консоль
+ + Прочитать слова из файла.
+ + Отсортировать в алфавитном порядке.
+ + Посчитать сколько раз каждое слово встречается в файле.
+ + Вывести статистику на консоль
  Найти слово с максимальным количеством повторений. Вывести на консоль это слово и сколько раз оно встречается в файле
  */
 
@@ -17,30 +16,55 @@ import java.util.*;
 public class Task_6_Collection {
 
     public static void main(String[] args) throws IOException {
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        Scanner scanner = new Scanner(new File("C:\\Users\\PMikhaylov\\IdeaProjects\\untitled\\src\\main\\java\\file.txt"));
+        ArrayList<String> words = new ArrayList<String>();
 
-    }
-
-
-    // считываем файл в Arralist
-    public static void getInformation() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("file.txt"));
-        Map<String, Integer> statistics = new HashMap<>();
         while (scanner.hasNext()) {
-            String word = scanner.useDelimiter ("\\s+").next();
+            String word = scanner.useDelimiter("\\s+").next();
+            words.add(word);
+        }
+        System.out.println(words);
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Collections.sort(words);
+        System.out.println(words);
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        Scanner scanner1 = new Scanner(new File("C:\\Users\\PMikhaylov\\IdeaProjects\\untitled\\src\\main\\java\\file.txt"));
+        Map<String, Integer> statistics = new HashMap<String, Integer>();
+        while (scanner1.hasNext()) {
+            String word = scanner1.useDelimiter("\\s+").next();
             Integer count = statistics.get(word);
             if (count == null) {
                 count = 0;
-
             }
             statistics.put(word, ++count);
         }
         System.out.println(statistics);
 
 
+        int temp, count;
+        for (int i = 0; i < words.size();)
+        {
+            temp = words.get(i);
+            count = 1;
+            while(++i < array.length && array[i] == temp)
+                ++count;
+            System.out.print(temp + "-" + count + " ");
+        }
+
 
 
     }
+}
 
-    }
+
+
+
+
+
+
 
 
